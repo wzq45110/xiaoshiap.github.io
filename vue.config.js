e14dbc669d62ad0a25565a8bc408807768ed0616
嘻哈小魏 <wzq4511@163.com>
2020-04-05 17:32:27 +0800

    123456

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,22 @@
 module.exports = {
   publicPath: './',
   outputDir: 'dist',
-  assetsDir: 'public'
+  assetsDir: 'public',
+  devServer: {
+    open: true, // 是否自动弹出浏览器页面
+    host: 'localhost',
+    port: '8080',
+    https: false,
+    hotOnly: false,
+    // 前端解决跨域 反向代理
+    proxy: {
+      '/api': {
+        target: 'http://test.tuolve.com/bawang/web/tl_api.php?s=/', // API服务器的地址
+        changeOrigin: true,
+        pathRewrite: {
+          '^/api': ''
+        }
+      }
+    }
+  }
 }
